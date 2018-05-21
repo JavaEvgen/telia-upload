a086f84a7b9f6478fe3c37b9ea3852feea245590
JavaEvgen <evgeny0191@gmail.com>
2018-05-21 15:01:26 +0300

    Playing around with Dropbox JS SDK and API

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -1,8 +1,60 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+      items: []
+    };
+    //Binding fetch function to component's this
+    this.fetchFiles = this.fetchFiles.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchFiles();
+  }
+
+  fetchFiles() {
+    axios.get('/list')
+    .then((response) => {
+      var items = response.data.entries;
+      //Wait for all nested calls to finish
+      return Promise.all(items.map((item, index) => {
+        return axios.get('/download'+ item.path_lower)
+          .then((response) => {
+            item.link = response.data;
+            return item
+          });
+      }));     
+    })
+    //Pass a callback to update state
+    .then(items => this.setState(prevState => ({
+        isLoaded: true,
+        items: items
+      })))
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+
   render() {
-    return <h1>Hello Kris!</h1>;
+    const { isLoaded, items } = this.state;
+    if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li key={item.id}>
+              <a href={item.link}>{item.name}</a>
+            </li>
+          ))}
+        </ul>
+      );
+    }
   }
 }
 
--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,35 @@
 var express = require('express');
 var app = express();
 
+//Dropbox variables and dependencies
+var fs = require('fs');
+require('isomorphic-fetch');
+var Dropbox = require('dropbox').Dropbox;
+var dbx = new Dropbox({ accessToken: 'yXENnI3OBYgAAAAAAAAM-L2uNBSkYlq6HtfHiZXCUKuyA9EaNs5yWbU7tANOLOo3' });
+
 app.use(express.static(__dirname +'./../../')); //serves the index.html
+
+//Routes
+//Get list of all uploaded files
+app.get('/list', function(req, res) {
+    dbx.filesListFolder({path: ''})
+        .then(function(response) {
+            res.json(response);
+        })
+        .catch(function(error) {
+            res.json(error);
+        });
+});
+//Get temporary download link for a single file
+app.get('/download/:path', function(req, res) {
+    var path = '/' + req.params.path;
+    dbx.filesGetTemporaryLink({path: path})
+    .then(function(data) {
+      res.json(data.link)
+    })
+    .catch(function(error) {
+      console.error(error);
+    });
+});
+
 app.listen(3000); //listens on port 3000 -> http://localhost:3000/
